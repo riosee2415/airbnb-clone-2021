9801a9c7d3a7701ad6652025f2dbe689a12871db
4LEAF.YSH <4leaf.ysh@gmail.com>
2021-01-21 00:30:17 +0900

    users model part_1

---
+++ users/models.py
@@ -0,0 +1,37 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+
+    """ Custom User Model """
+
+    GENDER_MALE = "male"
+    GENDER_FEMALE = "female"
+    GENDER_OTHER = "other"
+
+    GENDER_CHOICES = (
+        (GENDER_MALE, "Male"),
+        (GENDER_FEMALE, "Female"),
+        (GENDER_OTHER, "Other"),
+    )
+
+    LANGUAGE_ENGLISH = "en"
+    LANGUAGE_KOREAN = "kr"
+
+    LANUAGE_CHOICES = ((LANGUAGE_ENGLISH, "English"), (LANGUAGE_KOREAN, "Korean"))
+
+    CURRENCY_USD = "usd"
+    CURRENCY_KRW = "krw"
+
+    CURRENTCY_CHOICES = ((CURRENCY_USD, " USD"), (CURRENCY_KRW, " KRW"))
+
+    avatar = models.ImageField(null=True, blank=True)
+    gender = models.CharField(
+        choices=GENDER_CHOICES, max_length=10, null=True, blank=True
+    )
+    bio = models.TextField(default="", blank=True)
+    birthdate = models.DateField(null=True)
+    language = models.CharField(choices=LANUAGE_CHOICES, null=True, max_length=2)
+    curreny = models.CharField(choices=CURRENTCY_CHOICES, null=True, max_length=3)
+    superhost = models.BooleanField(default=False)
